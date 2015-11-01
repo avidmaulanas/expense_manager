12367fe5c8260ae64bfd2131681cf4a0d9da0755
Avid Maulana Saputra <avidmaulanas@gmail.com>
2015-11-01 07:37:00 +0700

    Fixed omniauth callback

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,28 +2,26 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   before_action :set_user
 
   def facebook
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    omniauth_callback :facebook
   end
 
   def twitter
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
-    else
-      session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
-    end
+    omniauth_callback :twitter
   end
 
   private
     def set_user
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"])
+    end
+
+    def omniauth_callback(provider)
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+        set_flash_message(:notice, :success, kind: provider.to_s.titleize) if is_navigational_format?
+      else
+        session["devise.#{provider.to_s}_data"] = provider.eql?(:facebook) ? request.env["omniauth.auth"] : request.env["omniauth.auth"].except("extra")
+        redirect_to new_user_registration_url
+      end
     end
 end
